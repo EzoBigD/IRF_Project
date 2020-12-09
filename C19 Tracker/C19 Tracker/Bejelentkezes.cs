150d0262ee24d66c531b374c07ce7ecc3c545dff
Ádám Ezzeddine <adam.ezzeddine@stud.uni-corvinus.hu>
2020-12-09 16:29:52 +0100

    3/ Adatbázis hozzáadása a projekthez, Két form kreaálása

--- C19 Tracker/C19 Tracker/Bejelentkezes.cs
+++ C19 Tracker/C19 Tracker/Bejelentkezes.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace C19_Tracker
 {
-    public partial class Form1 : Form
+    public partial class Bejelentkezes : Form
     {
-        public Form1()
+        public Bejelentkezes()
         {
             InitializeComponent();
         }
